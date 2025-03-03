46d554a6803d2808df35b0b64e12fa6fb34fb01d
moktadir-mirul <moktadir.mirul@gmail.com>
2025-03-03 15:49:57 +0600

    on click color and image change done

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ for (let i = 0; i < colorButtons.length; i++) {
     const clrBtn = colorButtons[i];
     clrBtn.addEventListener("click", function(event) {
         const clickedBtn = event.target;
-        console.log(clickedBtn, "hello2");
         for(let j = 0; j< colorButtons.length; j++) {
             colorButtons[j].classList.remove('border-purple-700');
+            colorButtons[j].classList.add("border-gray-300");
         }
         clickedBtn.classList.add('border-purple-700');
+        clickedBtn.classList.remove("border-gray-300");
+         const productImage = document.getElementById("product-image");
+         const color = clickedBtn.id.replace("-color", ''); 
+         productImage.src = "./images/" + color +".png";
     })
       
 }
